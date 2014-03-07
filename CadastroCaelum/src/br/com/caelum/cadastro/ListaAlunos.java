4b529f3fa2b61c2536717eaf6d6e4ccbf3d20383
eriko <erikomorais@gmail.com>
2014-03-07 10:28:27 -0300

    exercicios

--- CadastroCaelum/src/br/com/caelum/cadastro/ListaAlunos.java
+++ CadastroCaelum/src/br/com/caelum/cadastro/ListaAlunos.java
@@ -1,7 +1,11 @@
 package br.com.caelum.cadastro;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -16,10 +20,10 @@ public class ListaAlunos extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listagem_alunos);
-		ListView lista = (ListView) findViewById(R.id.lista);
+		ListView lista = (ListView) findViewById(R.id.lista_alunos);
 		
 		int layout = android.R.layout.simple_list_item_1;
-		String[] nomes = {"Ana","Jose","Felipe"};
+		String[] nomes = {"s","Jose","Felipe"};
 		ArrayAdapter<String> adapter =  new ArrayAdapter<String>(this, layout, nomes);
 		
 		lista.setAdapter(adapter);
@@ -50,6 +54,29 @@ public class ListaAlunos extends Activity {
 	
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.lista_alunos, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	
+		int itemClicado = item.getItemId(); 
+		switch (itemClicado) {
+		case R.id.novo:
+			Intent irParaFormulario =  new Intent(this,CreateAluno.class);
+			startActivity(irParaFormulario);
+			break;
+
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 	
 
 }
+ 
