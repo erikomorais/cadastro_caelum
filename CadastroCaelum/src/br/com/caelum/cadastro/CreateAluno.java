592f623700b7dfc4c99a8622be6d7f21c6fa2db6
eriko <erikomorais@gmail.com>
2014-03-10 14:43:42 -0300

    A

--- CadastroCaelum/src/br/com/caelum/cadastro/CreateAluno.java
+++ CadastroCaelum/src/br/com/caelum/cadastro/CreateAluno.java
@@ -1,15 +1,33 @@
 package br.com.caelum.cadastro;
 
+import br.com.caelum.cadastro.modelo.Aluno;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class CreateAluno extends Activity{
 
+	private CadastroHelper helper;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 	
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.cadastro);
+		this.helper = new CadastroHelper(this);
+		
+		Button botao = (Button)findViewById(R.id.botao);
+		botao.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Aluno aluno = helper.pegaAlunoDoFormulario();
+				Toast.makeText(CreateAluno.this, aluno.getNome(), Toast.LENGTH_SHORT).show();
+				
+			}
+		});
 	}
 
 }
--- CadastroCaelum/src/br/com/caelum/cadastro/ListaAlunos.java
+++ CadastroCaelum/src/br/com/caelum/cadastro/ListaAlunos.java
@@ -16,72 +16,71 @@ import android.widget.Toast;
 
 public class ListaAlunos extends Activity {
 
-	
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listagem_alunos);
 		ListView lista = (ListView) findViewById(R.id.lista_alunos);
-		
+
 		int layout = android.R.layout.simple_list_item_1;
-		String[] nomes = {"s","Jose","Felipe"};
-		ArrayAdapter<String> adapter =  new ArrayAdapter<String>(this, layout, nomes);
-		
+		String[] nomes = { "s", "Jose", "Felipe" };
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, layout,
+				nomes);
+
 		lista.setAdapter(adapter);
-		lista.setOnItemClickListener(  new OnItemClickListener() {
+		lista.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> adapter, View view, int posicao,long id) {
-				Toast.makeText(ListaAlunos.this, "Clique na posição"+posicao, Toast.LENGTH_SHORT).show();
-				
+			public void onItemClick(AdapterView<?> adapter, View view,
+					int posicao, long id) {
+				Toast.makeText(ListaAlunos.this, "Clique na posição" + posicao,
+						Toast.LENGTH_SHORT).show();
+
 			}
-			
+
 		});
-		
-		lista.setOnItemLongClickListener(  new OnItemLongClickListener() {
+
+		lista.setOnItemLongClickListener(new OnItemLongClickListener() {
 
 			@Override
-			public boolean onItemLongClick(AdapterView<?> adapter, View view, int posicao, long id) {
-				
-				Toast.makeText(ListaAlunos.this, "Clique longo no item "+ adapter.getItemAtPosition(posicao), Toast.LENGTH_SHORT).show();
+			public boolean onItemLongClick(AdapterView<?> adapter, View view,
+					int posicao, long id) {
+
+				Toast.makeText(
+						ListaAlunos.this,
+						"Clique longo no item "
+								+ adapter.getItemAtPosition(posicao),
+						Toast.LENGTH_SHORT).show();
 				return true;
 			}
 
-			
-			
 		});
-		
-		
-	
+
 	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		
+
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu_principal, menu);
 		// TODO Auto-generated method stub
 		return super.onCreateOptionsMenu(menu);
-		
+
 	}
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-        case R.id.menu_novo:
+		case R.id.menu_novo:
 
-            Intent intent = new Intent(ListaAlunos.this, 
-                CreateAluno.class);
+			Intent intent = new Intent(ListaAlunos.this, CreateAluno.class);
 
-            startActivity(intent);
-            return false;
-    default:
-        return super.onOptionsItemSelected(item);
+			startActivity(intent);
+			return false;
+		default:
+			return super.onOptionsItemSelected(item);
 
-	
-	
+		}
 	}
-	}
-	
 
 }
- 
