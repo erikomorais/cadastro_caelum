689f9cc4f02670d35b731fa2b8bbc0fb0a81066d
eriko <erikomorais@gmail.com>
2014-03-14 11:49:54 -0300

    exercicio 7

--- CadastroCaelum/src/br/com/caelum/cadastra/dao/AlunoDao.java
+++ CadastroCaelum/src/br/com/caelum/cadastra/dao/AlunoDao.java
@@ -1,5 +1,8 @@
 package br.com.caelum.cadastra.dao;
 
+import br.com.caelum.cadastro.modelo.Aluno;
+import android.app.DownloadManager.Query;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
@@ -7,32 +10,42 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class AlunoDao extends SQLiteOpenHelper {
 	private static final int VERSAO = 1;
-	private static final String TABELA = "CadastroCaelum";
-	private static final String DATABASE = "FJ57";
-	
+	private static final String TABELA = "FJ57";
+	private static final String DATABASE = "CadastroCaelum";
+
 	public AlunoDao(Context context) {
-		super(context, DATABASE,null ,1);
+		super(context, DATABASE, null, 1);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		String ddl = "CREATE TABLE FJ57 ("
-				+ "id INTEGER PRIMARY KEY, "
-				+ "nome TEXT UNIQUE NOT NULL, "
-				+ "telefone TEXT, "
-				+ "endereco TEXT, "
-				+ "site TEXT, "
-				+ "nota REAL, "
-				+ "foto TEXT);";
-		
+		String ddlCreateTable = "CREATE TABLE "+ TABELA 
+				+ "(id INTEGER PRIMARY KEY, " + "nome TEXT UNIQUE NOT NULL, "
+				+ "telefone TEXT, " + "endereco TEXT, " + "site TEXT, "
+				+ "nota REAL, " + "foto TEXT);";
+		db.execSQL(ddlCreateTable);
+
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
+		String ddlDropTable = "DROP TABLE IF EXISTS "+ TABELA +";";
+		db.execSQL(ddlDropTable);
+		this.onCreate(db);
+
+	}
+	
+	public void insere(Aluno aluno){
+		ContentValues values = new ContentValues();
+		values.put("nome", aluno.getNome());
+		values.put("nota", aluno.getNota());
+		values.put("telefone", aluno.getTelefone());
+		values.put("endereco", aluno.getEndereco());
+		values.put("site", aluno.getSite());
+		values.put("nota", aluno.getNota());
+		getWritableDatabase().insert(TABELA, null, values);
 		
 	}
 
-	
 }
